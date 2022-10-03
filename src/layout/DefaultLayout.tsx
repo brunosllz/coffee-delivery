2d0918de3a0e74444f5bb832f68d0c730f1e2974
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-03 13:06:12 -0300

    Add link for navigation

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 import { MapPin, ShoppingCartSimple } from 'phosphor-react'
 import LogoImage from '../assets/logo.svg'
@@ -6,9 +6,11 @@ import LogoImage from '../assets/logo.svg'
 export function DefaultLayout() {
   return (
     <div>
-      <header className="flex py-11 bg-gray-100 fixed w-full ">
+      <header className="flex py-11 bg-gray-100 w-full ">
         <div className="flex items-center w-full max-w-[1120px] mx-auto justify-between">
-          <img src={LogoImage} alt="" />
+          <Link to="/">
+            <img src={LogoImage} alt="" />
+          </Link>
 
           <div className="flex items-center justify-center gap-3">
             <div className="flex bg-purple-300 rounded-md items-center gap-1 text-purple-500 p-2">
@@ -16,9 +18,12 @@ export function DefaultLayout() {
               <span>Porto Alegre, RS</span>
             </div>
 
-            <button className="w-38 h-38 bg-yellow-300 text-yellow-500 p-2 rounded-md">
+            <Link
+              to="checkout"
+              className="w-38 h-38 bg-yellow-300 text-yellow-500 p-2 rounded-md"
+            >
               <ShoppingCartSimple width={22} weight="fill" />
-            </button>
+            </Link>
           </div>
         </div>
       </header>
