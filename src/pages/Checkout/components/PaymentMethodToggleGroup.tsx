d37c9832c63057011dbbd03de99f7ff87a52bc7e
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-04 12:55:15 -0300

    Refactoring payment method toggle group

--- src/pages/Checkout/components/PaymentMethodToggleGroup.tsx
+++ src/pages/Checkout/components/PaymentMethodToggleGroup.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 import { useController, Control, FieldValues, Path } from 'react-hook-form'
 import * as ToggleGroup from '@radix-ui/react-toggle-group'
-import { Horse } from 'phosphor-react'
+import { CreditCard, Bank, Money } from 'phosphor-react'
 
 interface PaymentMethod {
   id: string
@@ -10,9 +10,21 @@ interface PaymentMethod {
 }
 
 const paymentMethods: PaymentMethod[] = [
-  { id: '1', icon: <Horse />, title: 'Domingo' },
-  { id: '2', icon: <Horse />, title: 'Segunda' },
-  { id: '3', icon: <Horse />, title: 'Terça' },
+  {
+    id: '1',
+    icon: <CreditCard size={16} className="text-purple-500" />,
+    title: 'Cartão de crédito',
+  },
+  {
+    id: '2',
+    icon: <Bank size={16} className="text-purple-500" />,
+    title: 'Cartão de débito',
+  },
+  {
+    id: '3',
+    icon: <Money size={16} className="text-purple-500" />,
+    title: 'Dinheiro',
+  },
 ]
 
 interface WeekDayslInputControllProps<T extends FieldValues = FieldValues> {
@@ -32,7 +44,7 @@ export function PaymentMethodToggleGroup<T extends FieldValues = FieldValues>({
   return (
     <div className="flex w-full">
       <ToggleGroup.Root
-        type="multiple"
+        type="single"
         className="w-full flex grid-cols-3 gap-3"
         value={value}
         onValueChange={(newValue) => onChange(newValue)}
@@ -42,7 +54,7 @@ export function PaymentMethodToggleGroup<T extends FieldValues = FieldValues>({
             key={paymentMethod.id}
             value={paymentMethod.id}
             type="button"
-            className="flex items-center justify-center gap-3 [&[data-state='on']]:bg-violet-500  w-full p-4 rounded-md bg-purple-300 transition-colors hover:bg-gray-500"
+            className="flex items-center text-xs justify-center gap-3 [&[data-state='on']]:bg-purple-300  w-full p-4 rounded-md bg-gray-400 transition-colors hover:bg-gray-500 focus:ring-purple-500"
           >
             {paymentMethod.icon}
             {paymentMethod.title}
