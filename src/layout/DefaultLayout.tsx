f8e6fb3408fe88bea9a1ba2165f8028e7147ce84
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-05 12:37:09 -0300

    Add indicator show how much itens  have into shooping cart

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -2,11 +2,17 @@ import { Link, Outlet } from 'react-router-dom'
 
 import { MapPin, ShoppingCartSimple } from 'phosphor-react'
 import LogoImage from '../assets/logo.svg'
+import { useContext } from 'react'
+import { ShoopingCartContext } from '../contexts/ShoopingCartContext'
 
 export function DefaultLayout() {
+  const { selectedCoffies } = useContext(ShoopingCartContext)
+
+  const hasItemInShoopingCart = selectedCoffies.length > 0
+
   return (
     <div>
-      <header className="flex py-11 bg-gray-100 w-full ">
+      <header className="flex py-11 bg-gray-100 w-full">
         <div className="flex items-center w-full max-w-[1120px] mx-auto justify-between">
           <Link to="/">
             <img src={LogoImage} alt="" />
@@ -20,8 +26,13 @@ export function DefaultLayout() {
 
             <Link
               to="checkout"
-              className="w-38 h-38 bg-yellow-300 text-yellow-500 p-2 rounded-md"
+              className="w-38 h-38 bg-yellow-300 text-yellow-500 p-2 rounded-md relative"
             >
+              {hasItemInShoopingCart && (
+                <div className="absolute -top-2 -right-2 w-5 h-5 rounded-full flex items-center justify-center bg-yellow-700 text-white text-xs">
+                  {selectedCoffies.length}
+                </div>
+              )}
               <ShoppingCartSimple width={22} weight="fill" />
             </Link>
           </div>
