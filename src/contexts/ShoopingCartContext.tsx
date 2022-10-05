5df72e6ac17ea15a67a4e5bc583ec4f54419382d
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-05 07:41:58 -0300

    Refactoring shooping cart context, add updated and remove coffee function

--- src/contexts/ShoopingCartContext.tsx
+++ src/contexts/ShoopingCartContext.tsx
@@ -1,17 +1,33 @@
 import { createContext, ReactNode, useState } from 'react'
 
-interface AddItemtoShoopingCartData {
+interface CoffeeAmount {
   id: string
   amount: number
+}
+
+interface AddItemtoShoopingCartData {
+  id: string
   description: string
   name: string
   imageUrl: string
   price: number
+  coffeeAmount: CoffeeAmount
+}
+
+interface UpdateCoffeeCheckout {
+  id: string
+  description: string
+  name: string
+  imageUrl: string
+  price: number
+  coffeeAmount: CoffeeAmount
 }
 
 interface ShoopingCartContextProps {
   addItemtoShoopingCart: (data: AddItemtoShoopingCartData) => void
   selectedCoffies: AddItemtoShoopingCartData[]
+  updateAmountCoffeeAtCheckout: (data: UpdateCoffeeCheckout[]) => void
+  removeCoffeeatCheckout: (coffeeId: string) => void
 }
 
 export const ShoopingCartContext = createContext({} as ShoopingCartContextProps)
@@ -33,7 +49,7 @@ export function ShoopingCartProvider({ children }: ShoopingCartProviderProps) {
     const searchCoffee = addCoffee.find((coffee) => coffee.id === data.id)
 
     if (searchCoffee) {
-      searchCoffee!.amount += data.amount
+      searchCoffee!.coffeeAmount.amount += data.coffeeAmount.amount
 
       return setSelectedCoffies(addCoffee)
     }
@@ -41,9 +57,26 @@ export function ShoopingCartProvider({ children }: ShoopingCartProviderProps) {
     setSelectedCoffies((state) => [...state, data])
   }
 
+  function updateAmountCoffeeAtCheckout(data: UpdateCoffeeCheckout[]) {
+    setSelectedCoffies(data)
+  }
+
+  function removeCoffeeatCheckout(coffeeId: string) {
+    const deleteCoffee = selectedCoffies.filter(
+      (coffee) => coffee.id !== coffeeId,
+    )
+
+    setSelectedCoffies(deleteCoffee)
+  }
+
   return (
     <ShoopingCartContext.Provider
-      value={{ addItemtoShoopingCart, selectedCoffies }}
+      value={{
+        addItemtoShoopingCart,
+        selectedCoffies,
+        updateAmountCoffeeAtCheckout,
+        removeCoffeeatCheckout,
+      }}
     >
       {children}
     </ShoopingCartContext.Provider>
